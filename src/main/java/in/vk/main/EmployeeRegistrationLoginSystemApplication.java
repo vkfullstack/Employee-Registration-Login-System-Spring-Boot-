f5f4d845b0efb1760003dc0e3f5959787b8ebf67
vkfullstack <vijaykhetre36@gmail.com>
2026-05-02 10:14:21 +0530

    Add full Employee Registration and Login Spring Boot project

---
+++ src/main/java/in/vk/main/EmployeeRegistrationLoginSystemApplication.java
@@ -0,0 +1,13 @@
+package in.vk.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationLoginSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistrationLoginSystemApplication.class, args);
+	}
+
+}
