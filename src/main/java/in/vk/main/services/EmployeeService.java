db640c94da9890f13d49c6d0f91191ac29750a18
vkfullstack <vijaykhetre36@gmail.com>
2026-05-02 09:56:23 +0530

    Implement EmployeeService and business logic for registration and login

---
+++ src/main/java/in/vk/main/services/EmployeeService.java
@@ -0,0 +1,8 @@
+package in.vk.main.services;
+
+import in.vk.main.entity.Employee;
+
+public interface EmployeeService {
+public boolean Empregister(Employee emp);
+public Employee Emplogin(String email,String password);
+}
